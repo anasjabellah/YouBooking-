86396998c48cb920394a52cf264c067833a70c37
anas jabellah <jablcodiing@gmail.com>
2023-01-03 06:48:13 +0100

    add contr hotel

--- ressources/HotelResource.java
+++ ressources/HotelResource.java
@@ -12,19 +12,20 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("/Hotels")
 public class HotelResource {
 
     @Autowired
     HotelService hotelService ;
 
-    @GetMapping(path = "/AllHotels")
+    @GetMapping(path = "/All")
     public ResponseEntity<List<Hotel>> getAllHotels(){
         List<Hotel> hotels = hotelService.GetAllHotels() ;
         return ResponseEntity.ok(hotels);
     }
 
-    @GetMapping("/Hotel/{id}")
+    @GetMapping("/find/{id}")
     public ResponseEntity<Hotel> GetResById(@PathVariable("id") long id){
         Optional<Hotel> HotelId = hotelService.findById(id);
         if(HotelId.isPresent()){
@@ -35,15 +36,21 @@ public class HotelResource {
     }
 
 
-    @PostMapping("/Hotel/{idManadger}")
+    @PostMapping("/add/{idManadger}")
     public Hotel save(@RequestBody Hotel hotel , @PathVariable Long idManadger) {
         return hotelService.addHotel(hotel,idManadger);
     }
 
 
-    @PutMapping("/Hotel/updet/{idHotel}")
+    @PutMapping("/update/{idHotel}")
     public Hotel update(@RequestBody Hotel hotel , @PathVariable Long idHotel) {
         return hotelService.updateHotel(hotel , idHotel);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?>  deletHotel(@PathVariable("id") Long id){
+        hotelService.DeleteHotel(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
